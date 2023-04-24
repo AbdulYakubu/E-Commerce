ae05ebc32db9de59e69dddd6f90f3228d8f7317d
Aziz Abdul Yakubu <yakubuabdulaziz641@gmail.com>
2023-04-24 08:23:30 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Glams Closet</a>
+      </header>
+      <main>List of Products</main>
+    </div>
+  );
+}
+
+export default App;
